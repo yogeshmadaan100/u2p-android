be3168ea778c4516223b3c91f9fc24b53093ff47
almartinflorido@gmail.com <almartinflorido@gmail.com@445c90af-288d-6f11-3184-83c328532895>
2012-11-22 15:08:35 +0000

    Añadido ListView con Item personalizados Añadidos iconos en distintos tamaños Añadido clase ItemFile

--- U2P/src/com/u2p/ui/MainActivity.java
+++ U2P/src/com/u2p/ui/MainActivity.java
@@ -1,21 +1,22 @@
 package com.u2p.ui;
 
+import java.util.ArrayList;
+
 import android.app.ActionBar;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.NavUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.u2p.ui.adapters.ItemFileAdapter;
+import com.u2p.ui.component.ItemFile;
 
 public class MainActivity extends FragmentActivity implements ActionBar.OnNavigationListener {
 
@@ -40,13 +41,38 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
                         android.R.id.text1,
                         new String[]{
                                 getString(R.string.title_section1),
-                                getString(R.string.title_section2),
-                                getString(R.string.title_section3),
+                                //getString(R.string.title_section2),
+                                //getString(R.string.title_section3),
                         }),
                 this);
         
+        ListView lv = (ListView) findViewById(R.id.listView);
+        ArrayList<ItemFile> items = obtenerItems();
+        ItemFileAdapter adapter = new ItemFileAdapter(this, items);
+        
+        lv.setAdapter(adapter);
+        
     }
 
+    private ArrayList<ItemFile> obtenerItems() {
+    	ArrayList<ItemFile> items = new ArrayList<ItemFile>();
+    	
+    	items.add(new ItemFile(1, "drawable/file", "archivo.txt", "arianjm", "12Kb", "15/20"));
+    	items.add(new ItemFile(2, "drawable/file", "archiv231o.txt", "almartin", "12Kb", "15/20"));
+    	items.add(new ItemFile(3, "drawable/file", "weq.txt", "almartin", "45633", "15/20"));
+    	items.add(new ItemFile(4, "drawable/file", "grgw.txt", "arian", "12Kb", "15/20"));
+    	
+    	items.add(new ItemFile(5, "drawable/file", "archivo.txt", "arianjm", "12Kb", "15/20"));
+    	items.add(new ItemFile(6, "drawable/file", "archiv231o.txt", "almartin", "12Kb", "15/20"));
+    	items.add(new ItemFile(7, "drawable/file", "weq.txt", "almartin", "45633", "15/20"));
+    	items.add(new ItemFile(8, "drawable/file", "grgw.txt", "arian", "12Kb", "15/20"));
+    	
+    	items.add(new ItemFile(9, "drawable/file", "archivo.txt", "arianjm", "12Kb", "15/20"));
+    	items.add(new ItemFile(10, "drawable/file", "archiv231o.txt", "almartin", "12Kb", "15/20"));
+    	items.add(new ItemFile(11, "drawable/file", "weq.txt", "almartin", "45633", "15/20"));
+    	items.add(new ItemFile(12, "drawable/file", "grgw.txt", "arian", "12Kb", "15/20"));
+    	return items;
+    }
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
@@ -72,13 +98,13 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
    // @Override
     public boolean onNavigationItemSelected(int position, long id) {
         // When the given tab is selected, show the tab contents in the container
-        Fragment fragment = new DummySectionFragment();
-        Bundle args = new Bundle();
-        args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-        fragment.setArguments(args);
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.container, fragment)
-                .commit();
+    	Fragment fragment = new DummySectionFragment();
+    	Bundle args = new Bundle();
+    	args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+    	fragment.setArguments(args);
+    	getSupportFragmentManager().beginTransaction()
+    		.replace(R.id.container, fragment)
+    		.commit();
         return true;
     }
 
@@ -97,7 +123,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.OnNaviga
             TextView textView = new TextView(getActivity());
             textView.setGravity(Gravity.CENTER);
             Bundle args = getArguments();
-            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
+           // textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)));
             return textView;
         }
     }
---
+++ U2P/src/com/u2p/ui/component/ItemFile.java
@@ -0,0 +1,85 @@
+package com.u2p.ui.component;
+
+public class ItemFile {
+	protected long id;
+	protected String rutaImagen;
+	protected String name;
+	protected String user;
+	protected String size;
+	protected String rating;
+	
+	public ItemFile(long id, String rutaImagen, String name, String user,
+			String size, String rating) {
+		this.id = id;
+		this.rutaImagen = rutaImagen;
+		this.name = name;
+		this.user = user;
+		this.size = size;
+		this.rating = rating;
+	}
+
+	public ItemFile(){
+		this.rutaImagen = "";
+		this.name="";
+		this.size="";
+		this.rating="";
+	}
+	
+	public ItemFile(long id, String rutaImagen, String name, String size, String rating) {
+		this.id = id;
+		this.rutaImagen = rutaImagen;
+		this.name = name;
+		this.size = size;
+		this.rating = rating;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getRutaImagen() {
+		return rutaImagen;
+	}
+
+	public void setRutaImagen(String rutaImagen) {
+		this.rutaImagen = rutaImagen;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSize() {
+		return size;
+	}
+
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
+}
