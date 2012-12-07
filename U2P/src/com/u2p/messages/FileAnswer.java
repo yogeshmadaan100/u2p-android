6b9756cd0db0d2408fccf438e68eda9e8a90ce02
arianjm <arianjm@gmail.com>
2012-12-07 11:40:05 +0000

    Mensajes

--- U2P/src/com/u2p/messages/FileAnswer.java
+++ U2P/src/com/u2p/messages/FileAnswer.java
@@ -1,12 +1,44 @@
 package com.u2p.messages;
 
+import java.io.File;
 import java.io.Serializable;
 
 public class FileAnswer implements Serializable{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
+	private String requestedFileURI;
+	private File file;
+	private int fileSize;
+	
+	public FileAnswer(String requestedFileURI, File file, int fileSize){
+		setRequestedFileURI(requestedFileURI);
+		setFile(file);
+		setFileSize(fileSize);
+	}
+
+	public String getRequestedFileURI() {
+		return requestedFileURI;
+	}
+
+	public void setRequestedFileURI(String requestedFileURI) {
+		this.requestedFileURI = requestedFileURI;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public int getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(int fileSize) {
+		this.fileSize = fileSize;
+	}
+	
 }
