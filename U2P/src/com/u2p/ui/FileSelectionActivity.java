bfd6d7a9051584adf070ced603c03487bc619800
arianjm@gmail.com <arianjm@gmail.com@445c90af-288d-6f11-3184-83c328532895>
2012-12-08 12:28:59 +0000

    File Selection mejorado Mejorada la activity de selección de ficheros para compartir

--- U2P/src/com/u2p/ui/FileSelectionActivity.java
+++ U2P/src/com/u2p/ui/FileSelectionActivity.java
@@ -1,9 +1,8 @@
 package com.u2p.ui;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
-
-import com.u2p.core.db.DbDataSource;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -13,16 +12,33 @@ import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.u2p.core.db.DbDataSource;
 
 public class FileSelectionActivity extends Activity {
 
-	private ArrayList<File> mFileList = new ArrayList<File>();
-    private File mPath = new File(Environment.getExternalStorageDirectory() + DbDataSource.getMaindir());
     private static final String TAG = "FileSelection";
     private static final String FILES_TO_UPLOAD = "upload";
+    private File mainPath = new File(Environment.getExternalStorageDirectory() + DbDataSource.getMaindir());
     private ArrayList<File> resultFileList;
+    
+    private ListView directoryView;
+    private ArrayList<File> directoryList = new ArrayList<File>();
+    private ArrayList<String> directoryNames = new ArrayList<String>();
+    private ListView fileView;
+	private ArrayList<File> fileList = new ArrayList<File>();
+	private ArrayList<String> fileNames = new ArrayList<String>();
+	
+	private ArrayList<File> definitiveList = new ArrayList<File>();
+	private ArrayList<String> definitiveStringList = new ArrayList<String>();
+
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,14 +46,34 @@ public class FileSelectionActivity extends Activity {
         setContentView(R.layout.activity_file_selection);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        loadFileList();
+        directoryView = (ListView)findViewById(R.id.directorySelectionList);
+        fileView = (ListView)findViewById(R.id.fileSelectionList);
+        TextView goUpView = (TextView)findViewById(R.id.goUpTextView);
+        goUpView.setClickable(true);
         
-        ArrayAdapter adapter = new ArrayAdapter<File>(this, android.R.layout.simple_list_item_multiple_choice, mFileList);
-        ListView lv = (ListView)findViewById(R.id.fileSelectionList);
-        lv.setAdapter(adapter);        
+        loadLists();
+        
+        directoryView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				File directory = (File) directoryList.get(position);
+				mainPath = directory;
+				loadLists();
+			}
+		});
+    }
+
+    public void onGoUpClickListener(View v){
+    	File parent = mainPath.getParentFile();
+    	Log.d(TAG, parent.toString());
+    	if(mainPath.equals(Environment.getExternalStorageDirectory())){
+    		Toast.makeText(this, "Can't exit external storage", Toast.LENGTH_SHORT).show();
+    	}else{
+    		mainPath = parent;
+    		loadLists();
+    	}
     }
     
-    public void onUploadClick(MenuItem item){
+	public void onUploadClick(MenuItem item){
     	Log.d(TAG, "Upload clicked, finishing activity");
         
     	ListView lv = (ListView)findViewById(R.id.fileSelectionList);
@@ -45,7 +81,7 @@ public class FileSelectionActivity extends Activity {
         
         for(int i = 0 ; i < lv.getCount() ; i++){
         	if(lv.isItemChecked(i)){
-        		resultFileList.add((File)lv.getItemAtPosition(i));
+        		resultFileList.add(fileList.get(i));
         	}
         }
         Log.d(TAG, "Files: "+resultFileList.toString());
@@ -54,35 +90,45 @@ public class FileSelectionActivity extends Activity {
         setResult(Activity.RESULT_OK, result);
     	finish();
     }
-
-    private void loadFileList() {
-        try {
-        	if(mPath.exists())
-        		Log.d(TAG, "Dir exists");
-        	else{
-        		if(mPath.mkdirs())
-        			Log.d(TAG, "Dir created");
-        		else
-        			Log.d(TAG, "Dir not created");
-        	}
-        }
-        catch(SecurityException e) {
-            Log.e(TAG, "unable to write on the sd card " + e.toString());
-        }
-        if(mPath.exists()) {
-        	if(mPath.length()>0){
-        		File[] tempList = mPath.listFiles();
-        		for(File file : tempList)
-        			if(file.isFile())
-        				mFileList.add(file);
-        		
-        		Log.d(TAG, "File list created");
-        	}
-        }
-        else {
-            Log.e(TAG, "File list not created");
-        }
-    }
+	
+	private void loadLists(){
+		FileFilter fileFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return file.isFile();
+			}
+		};
+		FileFilter directoryFilter = new FileFilter(){
+			public boolean accept(File file){
+				return file.isDirectory();
+			}
+		};
+		
+		if(mainPath.exists() && mainPath.length()>0){
+			//Lista de directorios
+			File[] tempDirectoryList = mainPath.listFiles(directoryFilter);
+			directoryList = new ArrayList<File>();
+			directoryNames = new ArrayList<String>();
+			for(File file: tempDirectoryList){
+				directoryList.add(file);
+				directoryNames.add(file.getName());
+			}
+			ArrayAdapter<String> directoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, directoryNames);
+	        directoryView.setAdapter(directoryAdapter);
+	        
+			//Lista de ficheros
+			File[] tempFileList = mainPath.listFiles(fileFilter);
+			fileList = new ArrayList<File>();
+    		fileNames = new ArrayList<String>();
+    		for(File file : tempFileList){
+    			fileList.add(file);
+    			fileNames.add(file.getName());
+    		}
+    		
+    		ArrayAdapter<String> fileAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, fileNames);
+            fileView.setAdapter(fileAdapter);
+            Log.d(TAG, "Lists created");
+		}
+	}
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
